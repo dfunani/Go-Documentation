d979ad2f80ab9cf8c284d49d2a8163cf66a1e15b
dfunani <dfunani@live.co.za>
2025-01-16 15:34:47 +0200

    Chapter 1 and 2

--- Basics/main.go
+++ Basics/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello", "World")
+	const str = `Hello 'Again'`
+	var defaultStr string
+
+	var ints = 1
+	var defaultInts int64 // uint8 == byte
+
+	var floats float64 = 1
+	var defaultFloats float64
+
+	var char = 'a'
+	var defaultChar rune
+
+	var toggle = true
+	var defaultToggle bool
+
+	fmt.Println(float64(defaultFloats + floats))
+	fmt.Println("Hello,", str, defaultStr, ints, defaultInts, floats, defaultFloats, string(char), defaultChar, toggle, defaultToggle)
 }
